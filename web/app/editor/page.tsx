dda9603f80d4bc261393587f18e67dc2f531b46f
Matt Campbell <matthew.campbell@bucks.edu>
2026-06-14 09:44:27 -0400

    updated text on editor page

--- web/app/editor/page.tsx
+++ web/app/editor/page.tsx
@@ -12,9 +12,9 @@ export default function EditorPage() {
   return (
     <main className="mx-auto w-full max-w-4xl flex-1 p-8">
       <header className="mb-8">
-        <h1 className="font-display text-4xl text-parchment">Editor</h1>
+        <h1 className="font-display text-4xl text-parchment">Dungeon Master Mode</h1>
         <p className="mt-1 text-parchment/60">
-          Pick a module to browse its data.
+          Each card below connects you to a module. Default Module is where the bulk of the game is coded, you can view that module buy any edits you make will not be saved. Other modules like "Side Quests", "Maps and Buildings" are libraries that you can import into your own modules. The only modules you can edit or delete are the modules that you create. Other players can view and play your modules.
         </p>
       </header>
 
